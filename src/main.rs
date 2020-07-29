0ba10fbdf7ccbfc7b798e0968339e3753a798cf4
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2020-07-28 20:32:31 -0700

    Add a basic UM-32 impl (passes sandmark)

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,30 @@
-use std::{
-    env, fs,
-    io::{self, Read},
-    path::Path,
-};
+use std::{env, fs, io, path::Path};
 
 mod um32;
 
-fn as_mut_u8_slice(slice: &mut [u32]) -> &mut [u8] {
-    unsafe {
-        std::slice::from_raw_parts_mut(
-            slice.as_mut_ptr() as *mut u8,
-            slice.len() * std::mem::size_of::<u32>(),
-        )
-    }
-}
-
 fn read_into_vec_u32<P: AsRef<Path>>(path: &P) -> io::Result<Vec<u32>> {
-    let size = fs::metadata(path)?.len();
-    if size % 4 != 0 {
+    let data = fs::read(path)?;
+    if data.len() % 4 != 0 {
         panic!("Invalid UM program file");
     }
-    let mut result = vec![0; (size / 4) as usize];
-    fs::File::open(path)?.read(as_mut_u8_slice(&mut result))?;
+    let mut result = Vec::with_capacity((data.len() / 4) as usize);
+    let mut idx = 0;
+    while idx < data.len() {
+        let v = (data[idx] as u32) << 24
+            | (data[idx + 1] as u32) << 16
+            | (data[idx + 2] as u32) << 8
+            | data[idx + 3] as u32;
+        result.push(v);
+        idx += 4;
+    }
     Ok(result)
 }
 
-fn main()  -> io::Result<()> {
+fn main() -> io::Result<()> {
     if env::args().len() != 2 {
         eprintln!("Usage: um32 <UM program file>");
         std::process::exit(1);
     }
     let program = read_into_vec_u32(&env::args().nth(1).unwrap())?;
-    um32::run(program);
-    Ok(())
+    um32::run(program)
 }
--- src/um32.rs
+++ src/um32.rs
@@ -1,3 +1,137 @@
-pub fn run(program: Vec<u32>) {
-    println!("{}", program.len());
+use std::io::{self, Read, Write};
+
+pub fn run(program: Vec<u32>) -> io::Result<()> {
+    let mut data = vec![program; 1];
+    let mut ip = 0;
+    let mut registers = vec![0u32; 8];
+    let stdin_ = io::stdin();
+    let mut stdin = stdin_.lock();
+    let stdout_ = io::stdout();
+    let mut stdout = stdout_.lock();
+    loop {
+        let mut insn = data[0][ip];
+        ip += 1;
+        let op = insn >> 28;
+        match op {
+            // Conditional Move
+            0 => {
+                let c = insn & 0x7;
+                if registers[c as usize] != 0 {
+                    insn >>= 3;
+                    let b = insn & 0x7;
+                    insn >>= 3;
+                    let a = insn & 0x7;
+                    registers[a as usize] = registers[b as usize];
+                }
+            }
+            // Array Index
+            1 => {
+                let c = insn & 0x7;
+                insn >>= 3;
+                let b = insn & 0x7;
+                insn >>= 3;
+                let a = insn & 0x7;
+                registers[a as usize] =
+                    data[registers[b as usize] as usize][registers[c as usize] as usize];
+            }
+            // Array Amendment
+            2 => {
+                let c = insn & 0x7;
+                insn >>= 3;
+                let b = insn & 0x7;
+                insn >>= 3;
+                let a = insn & 0x7;
+                data[registers[a as usize] as usize][registers[b as usize] as usize] =
+                    registers[c as usize];
+            }
+            // Addition
+            3 => {
+                let c = insn & 0x7;
+                insn >>= 3;
+                let b = insn & 0x7;
+                insn >>= 3;
+                let a = insn & 0x7;
+                registers[a as usize] = registers[b as usize].wrapping_add(registers[c as usize]);
+            }
+            // Multiplication
+            4 => {
+                let c = insn & 0x7;
+                insn >>= 3;
+                let b = insn & 0x7;
+                insn >>= 3;
+                let a = insn & 0x7;
+                registers[a as usize] = registers[b as usize].wrapping_mul(registers[c as usize]);
+            }
+            // Division
+            5 => {
+                let c = insn & 0x7;
+                insn >>= 3;
+                let b = insn & 0x7;
+                insn >>= 3;
+                let a = insn & 0x7;
+                registers[a as usize] = registers[b as usize] / registers[c as usize];
+            }
+            // Not-And
+            6 => {
+                let c = insn & 0x7;
+                insn >>= 3;
+                let b = insn & 0x7;
+                insn >>= 3;
+                let a = insn & 0x7;
+                registers[a as usize] = !(registers[b as usize] & registers[c as usize]);
+            }
+            // Halt
+            7 => break,
+            // Allocation
+            8 => {
+                let c = insn & 0x7;
+                insn >>= 3;
+                let b = insn & 0x7;
+                let new_arr = vec![0; registers[c as usize] as usize];
+                data.push(new_arr);
+                registers[b as usize] = (data.len() - 1) as u32;
+            }
+            // Abandonment
+            9 => {
+                let c = insn & 0x7;
+                data[registers[c as usize] as usize].clear();
+            }
+            // Output
+            10 => {
+                let c = insn & 0x7;
+                let v = registers[c as usize] as u8;
+                stdout.write_all(&[v])?;
+                stdout.flush()?;
+            }
+            // Input
+            11 => {
+                let c = insn & 0x7;
+                let mut buf = [0; 1];
+                let res = stdin.read(&mut buf)?;
+                if res == 0 {
+                    registers[c as usize] = !0;
+                } else {
+                    registers[c as usize] = buf[0] as u32;
+                }
+            }
+            // Load Program
+            12 => {
+                let c = insn & 0x7;
+                insn >>= 3;
+                let b = insn & 0x7;
+                match registers[b as usize] {
+                    0 => {}
+                    v => data[0] = data[v as usize].clone(),
+                }
+                ip = registers[c as usize] as usize;
+            }
+            // Orthography
+            13 => {
+                let a = (insn >> 25) & 0x7;
+                registers[a as usize] = insn & 0b0000_0001_1111_1111_1111_1111_1111_1111;
+            }
+            _ => panic!("Invalid operation {}", op),
+        }
+    }
+    Ok(())
 }
